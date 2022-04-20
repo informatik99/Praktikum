7285f56008baef3eba2706e0a1fcdfb93be73765
tobi <tobibawa@protonmail.com>
2022-04-20 21:21:26 +0200

    added server capability, user interaction, some fixes

--- keyValStore.c
+++ keyValStore.c
@@ -2,15 +2,15 @@
 #include "keyValStore.h"
 #include "stdio.h"
 
-
-int inStore =0;//Speichert ab, wie viele KeyValues bereits im Store sind!
-KeyValue store[50];//Die Struktur muss nicht hier definiert werden, weil sue schon in keyValStore.h ist
+int inStore =0;                        // Speichert ab, wie viele KeyValues bereits im Store sind!
+#define KEY_VALUE_MAX_LENGTH 50        // maximale anzahl an keyValues
+KeyValue store[KEY_VALUE_MAX_LENGTH];  // Die Struktur muss nicht hier definiert werden,
+// weil sie schon in keyValStore.h ist
 
 int get(char *key, char *res){
-
     for(int i=0;i< inStore;i++){
         if(strcmp(store[i].key,key) == 0){
-            strcpy(res,store[i].value);
+            strncpy(res,store[i].value, MAX_VALUE_LENGTH);
             return 0;
         }
     }
@@ -21,22 +21,26 @@ int put(char *key, char *value){
 
     KeyValue new = {"",""};
 
-    strcpy(new.key,key);
-    strcpy(new.value,value);
+    strncpy(new.key,key,MAX_KEY_LENGTH);
+    strncpy(new.value,value,MAX_VALUE_LENGTH);
 
-    printf("\nKEY: %s",new.key);
-    printf("\nVAL: %s",new.value);
-    printf("\nÜbergabe nach PUT hat geklappt");
-
-    /*Geht den Store durch, um zu schauen, ob dieser Key schon einen Wert hat*/
+    // Geht den Store durch, um zu schauen, ob dieser Key schon einen Wert hat
     for(int i = 0;i< inStore;i++){
-        if(store[i].key == key){
+        if(strncmp(store[i].key,key, MAX_KEY_LENGTH) == 0){
             strcpy(store[i].value, value);
             return 1;
         }
     }
 
-    //Falls nicht -> wird das neue KeyValue-Objekt hier in den Store getan
+    // der Key ist nicht im Store, jetzt müssen wir noch checken,
+    // ob wir genügend Platz haben, sonst bekommen wir vielleicht einen
+    // BufferOverflow (IndexOutOfBoundsException)
+    if(inStore >= KEY_VALUE_MAX_LENGTH){
+        // kein Platz mehr
+        return -1;
+    }
+
+    //Falls nicht, wird das neue KeyValue-Objekt hier in den Store getan
     store[inStore++] = new;
     printf("\nNeues Paar in Store gespeichert.");
     printf("\nObjekte im Store: %i",inStore);
@@ -44,44 +48,48 @@ int put(char *key, char *value){
     return 0;
 }
 
+
 int del(char *key){
+    // falls es keinen eintrag gibt,
+    // gib einen fehler zurück
+    if(inStore == 0){
+        return -1;
+    }
 
-    //Gibt an, ob des gesuchte Key gefunden wurde und dass man dann die Werte zurückkopiert.
-    int found = 0;
-
-    for(int i=0;i<inStore; i++){
-
-
-        //Gefunden
-        if(strcmp(store[i].key,key) == 0 || found == 1) {
-
-            //wenn i+1=inStore ist, gibt es keine weiteren Einträge.
-            //Da wir im else kopieren, muss der letzte Storeeintrag auch überschrieben werden, weil es
-            //sonst einen KeyValue 2 mal gibt.
-            if (i + 1 == inStore) {
-                strcpy(store[i].key, "\0");
-                strcpy(store[i].value, "\0");
-
-                inStore--;
-                printf("\nObjekte im Store: %i",inStore);
-                return 1;
-
-            } else
-                store[i] = store[i + 1];
-                found = 1;
-            }
-
+    // finde die richtige position im store
+    int keyIndex = 0;
+    for(keyIndex=0; keyIndex<inStore; keyIndex++){
+        if(strcmp(store[keyIndex].key, key) == 0){
+            break;
         }
-    printf("\nObjekte im Store: %i",inStore);
-
-    return 0;
     }
 
-    void testarray(){
-        printf("Im Store sind:");
+    // falls es den key nicht gibt,
+    // gib einen fehler aus
+    if(keyIndex >= inStore){
+        return -2;
+    }
+
+    // tausche den letzten eintrag mit dem
+    // des gesuchten keys
+    KeyValue tmp = store[keyIndex];
+    store[keyIndex] = store[inStore-1];
+    store[inStore-1] = tmp;
+
+    // der jetzt letzte eintrag kann
+    // gelöscht werden
+    inStore--;
+
+    // alles in ordnung
+    return 1;
+}
+
+void printStore(){
+    printf("\nStore {\n");
     for(int i=0; i<inStore;i++){
-        printf("\n Key: %s und Wert: %s",store[i].key,store[i].value);
+        printf("(key:%s | value: %s)\n",store[i].key,store[i].value);
     }
+    printf("}");
 }
 
 
--- main.c
+++ main.c
@@ -1,28 +1,123 @@
 #include <stdio.h>
 #include <netinet/in.h>
 #include "keyValStore.h"
-#include "string.h"
+#include <assert.h>
+#include <string.h>
+#include <unistd.h>
+
+void testStoreDel(){
+    del("Blub");
+    del("Eins");
+    del("Noch irgendwas");
+
+    put("Eins", "hallo");
+    put("Noch irgendwas", "bla");
+
+    assert(del("Eins") == 1);
+    assert(del("Noch irgendwas") == 1);
+    assert(del("Blub") < 0); // das sollte schief gehen
+}
+
+void testStoreSimplePutGet(){
+    char key[MAX_KEY_LENGTH] = "name";
+    char val[MAX_VALUE_LENGTH] = "robert";
+
+    put(key,val);
+
+    char res[MAX_VALUE_LENGTH];
+    get(key, res);
+    assert(strcmp(val, res) == 0);
+}
+
+void testStore(){
+    testStoreDel();
+    testStoreSimplePutGet();
+}
+
+void userInteraction(int fileDescriptor){
+    int inputBuffSize = MAX_KEY_LENGTH + MAX_VALUE_LENGTH;
+    char inputBuff[inputBuffSize];
+
+    char keyBuff[MAX_KEY_LENGTH] = "";
+    char valBuff[MAX_VALUE_LENGTH] = "";
+    int status = 0;
+
+    while(read(fileDescriptor,inputBuff,inputBuffSize) > 0){
+        if(strncmp(inputBuff,"QUIT",4) == 0){
+            break;
+        } else if(sscanf(inputBuff,"PUT %s %s",keyBuff,valBuff) == 2){
+            status = put(keyBuff,valBuff);
+            if(status < 0){
+                dprintf(fileDescriptor,"PUT FAILED\n");
+            }
+        } else if(sscanf(inputBuff, "DEL %s",keyBuff) == 1){
+            status = del(keyBuff);
+            if(status < 0){
+                dprintf(fileDescriptor, "DEL failed\n");
+            }
+        } else if(sscanf(inputBuff, "GET %s", keyBuff)){
+            status = get(keyBuff, valBuff);
+            if(status < 0 ){
+                dprintf(fileDescriptor, "GET failed\n");
+            } else {
+                dprintf(fileDescriptor,"GET:%s:%s\n" ,keyBuff,valBuff);
+            }
+        }
+    }
+}
+
+
+#define SERVER_PORT 5678
 
 int main() {
-char key[50] = "EINS";
-char value[50] = "ICH";
-    char key2[50] = "ZWEI";
-    char value2[50] = "DU";
+    testStore();
 
-    put(key2,value2);
-    char back[50];
-    get(key2,back);
-
-    printf("\n Zurückgekommen: %s",back);
-
-    //falschen Löschen
-    if(del("EINS") == 0){
-        printf("\n falscher Gelöscht");
-    }
-    if(del("ZWEI") == 1){
-        printf("\n richtiger Gelöscht");
+    // der server braucht ein socket für die kommunikation
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if(serverSocket < 0){
+        fprintf(stderr,"failed to open socket\n");
+        return 3;
     }
 
-    testarray();
+    // nach dem Schließen unseres server programms,
+    // wäre der socket noch für längere Zeit gebunden
+    // das kann nerven, wenn man schnell den server wieder starten möchte
+    if(setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0){
+        fprintf(stderr,"setsockopt(SO_REUSEADDR) failed");
+        return 4;
+    }
+
+    // wir wollen eine TCP Verbindung mit unserem eigenen Port
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    // die server port nummer muss in network byte order übergeben werden
+    serverAddress.sin_port = htons(SERVER_PORT);
+
+    // jetzt können wir unseren socket mit unseren daten binden
+    const int bindingStatus = bind(serverSocket, (struct sockaddr*) &serverAddress,
+                                   sizeof (serverAddress));
+    // falls das nicht klappt...
+    if(bindingStatus < 0){
+        fprintf(stderr, "couldn't bind socket\n");
+        return 4;
+    }
+
+
+    // jetzt können wir anfangen auf Anfragen zu hören
+    int maxConnections = 5;
+    listen(serverSocket, maxConnections);
+
+    // gerade akzeptieren wir nur ein client
+    struct sockaddr_in clientAddress;
+    unsigned int clientAddressLen = sizeof(clientAddress);
+    int clientFileDescriptor = accept(serverSocket,
+                                      (struct sockaddr*) &clientAddress,
+                                      &clientAddressLen);
+
+    // jetzt kann der user damit interagieren
+    userInteraction(clientFileDescriptor);
+
+    close(clientFileDescriptor);
     return 0;
 }
