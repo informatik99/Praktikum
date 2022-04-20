7b523edcbf6f2cc90d60e74f4820755ddf864d63
tobi <tobibawa@protonmail.com>
2022-04-20 22:19:30 +0200

    added user interaction

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <netinet/in.h>
+#include <unistd.h>
 #include "keyValStore.h"
 #include "string.h"
 #include "assert.h"
@@ -34,9 +35,40 @@ void testStore(){
     testStoreSimplePutGet();
 }
 
+void userInteraction(int fileDescriptor){
+    int inputBuffSize = MAX_KEY_LENGTH + MAX_VALUE_LENGTH;
+    char inputBuff[inputBuffSize];
 
+    char keyBuff[MAX_KEY_LENGTH] = "";
+    char valBuff[MAX_VALUE_LENGTH] = "";
+    int status = 0;
+
+    while(read(fileDescriptor,inputBuff,inputBuffSize) > 0){
+        if(strncmp(inputBuff,"QUIT",4) == 0){
+            break;
+        } else if(sscanf(inputBuff,"PUT %s %s",keyBuff,valBuff) == 2){
+            status = put(keyBuff,valBuff);
+            if(status < 0){
+                dprintf(fileDescriptor,"PUT FAILED\n");
+            }
+        } else if(sscanf(inputBuff, "DEL %s",keyBuff) == 1){
+            status = del(keyBuff);
+            if(status < 0){
+                dprintf(fileDescriptor, "DEL failed\n");
+            }
+        } else if(sscanf(inputBuff, "GET %s", keyBuff)){
+            status = get(keyBuff, valBuff);
+            if(status < 0 ){
+                dprintf(fileDescriptor, "GET failed\n");
+            } else {
+                dprintf(fileDescriptor,"GET:%s:%s\n" ,keyBuff,valBuff);
+            }
+        }
+    }
+}
 
 int main() {
     testStore();
+    userInteraction(STDOUT_FILENO);
     return 0;
 }
